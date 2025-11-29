7abd8aa3c2a136ce6d605651bf8987bad3c4fba6
Lucasdealmeid <133092843+Lucasdealmeid@users.noreply.github.com>
2025-11-29 20:10:12 +0100

    Update index.php

--- lobinave-main/index.php
+++ lobinave-main/index.php
@@ -1,46 +1,3 @@
-<?php
-
-if (isset($_POST['enviar'])) {
-	
-  //Variáveis
-  $nome= $_POST['nome'];
-	$bi = $_POST['bi'];
-	$area = $_POST['area'];
-	$obs = $_POST['obs'];
-
-    $email = $_POST['email'];
-    $mensagem = "Saudações caro ouvinte, chamo-me ".$nome." portador do bilhete de identidade número: ".$bi.", em anexo está o meu currículo vitae. Venho candidatar-me para a área de: ".$area.". E venho a observar que: ".$obs;;
-    $data_envio = date('d/m/Y');
-    $hora_envio = date('H:i:s');
-
-  //Compo E-mail
-  $arquivo = "
-    <html>
-      <p><b>Nome: </b>$nome</p>
-      <p><b>E-mail: </b>$email</p>
-      <p><b>Mensagem: </b>$mensagem</p>
-      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
-    </html>
-  ";
-  
-  //Emails para quem será enviado o formulário
-  $destino = "lucaskiadikiadijoaoalmeida@gmail.com";
-  $assunto = "Submissão de Candidatura";
-
-  //Este sempre deverá existir para garantir a exibição correta dos caracteres
-  $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
-
-  //Enviar
-  mail($destino, $assunto, $arquivo, $headers);
-  
-  echo "<meta http-equiv='refresh' content='10;URL=../index.html'>";
-
-}
-?>
-
-
 <!DOCTYPE html>
 <html lang="pt">
 <head>
@@ -287,7 +244,7 @@ if (isset($_POST['enviar'])) {
     <section data-aos="zoom-in-down" id="recruitment" class="py-12 md:py-20 bg-blue-800 text-white">
         <div class="container mx-auto px-4 md:px-6">
             <h2 class="text-2xl md:text-3xl font-bold text-center mb-8 md:mb-16">Recrutamento Espontâneo</h2>
-            <form action="index.php" method="POST" id="recruitmentForm" class="max-w-2xl mx-auto">
+            <form action="email.php" method="POST" id="recruitmentForm" class="max-w-2xl mx-auto">
                 <div class="form-grid grid md:grid-cols-2 gap-4 md:gap-6 mb-6">
                     <div>
                         <label for="fullName" class="block mb-2">Nome completo</label>
@@ -434,4 +391,5 @@ if (isset($_POST['enviar'])) {
 </body>
 
 </html>
+
 
